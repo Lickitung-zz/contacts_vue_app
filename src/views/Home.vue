86f84352d1b877aab01451380c2d35b2e182ee8f
SIeep <brandon_n_evans@live.com>
2019-04-07 10:26:35 -0500

    add index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="contact in contacts">
+    <p>First Name: {{ contact.first_name }}</p>
+    <p>Middle Name: {{ contact.middle_name }}</p>
+    <p>Last Name: {{ contact.last_name}}</p>
+    <p>Phone Number: {{ contact.phone_number }}</p>
+    <p>Email: {{ contact.email }}</p>
+    <hr>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      contacts: []          
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {
+      this.contacts = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
